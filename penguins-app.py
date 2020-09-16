d3443fa6d83f7a0c02a0f8bd73729a822a8ce732
Konstantin Ivanov <69236482+TredNR@users.noreply.github.com>
2020-09-16 16:45:35 +0400

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -12,69 +12,104 @@ This app predicts the **Palmer Penguin** species!
 Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
 """)
 
-st.sidebar.header('User Input Features')
+import streamlit as st
+import datetime
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
+st.title ("Test using different possibilities")
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
+st.header ("some text")
+
+st.subheader ("littel text")
+
+st.text ('text')
+
+st.markdown ("### Bolt")
+
+st.success ("Successful")
+
+st.info("Information")
+
+st.warning("This is a warning")
+
+st.error ("This is an error")
+
+st.exception ("NameError ('name three not defined')")
+
+st.help(range)
+
+st.write("Test with write")
+
+st.write(range(10))
+
+from PIL import Image
+img = Image.open("C:/Users/tred1/PycharmProjects/pythonProject2/image.jpg")
+st.image(img, width=300, caption="Simple Image")
+
+if st.checkbox ("Show/Hide"):
+    st.text("Showing of Hiding Widget")
+
+status = st.radio ("What is your status", ("Active", "Inactive"))
+if status =='Active':
+    st.success("You are Active")
 else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
+    st.warning("Inactive, Active")
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-penguins_raw = pd.read_csv('penguins_cleaned.csv')
-penguins = penguins_raw.drop(columns=['species'])
-df = pd.concat([input_df,penguins],axis=0)
+occupation = st.selectbox("Your Occupation", ["Programmer", "DataScientist", "Doctor", "Businessman"])
+st.write("You selected this option ", occupation)
 
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-encode = ['sex','island']
-for col in encode:
-    dummy = pd.get_dummies(df[col], prefix=col)
-    df = pd.concat([df,dummy], axis=1)
-    del df[col]
-df = df[:1] # Selects only the first row (the user input data)
+location = st.multiselect("Where do you work?", ('London', 'New York', 'Moscow', 'Kiev'))
+st.write("You selected", len(location), 'location')
 
-# Displays the user input features
-st.subheader('User Input features')
+level = st.slider('What is your level?', 1.0,5.0)
 
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+st.button("Simple Button")
 
-# Reads in saved classification model
-load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
+if st.button("About"):
+    st.text("Maybe... streamlit is coll")
 
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
+firstname = st.text_input("Enter Your Firstname", "Type Here")
+if st.button("Submit", key=1):
+    result = firstname.title()
+    st.success(result)
+
+massage =st.text_area("Enter Your massage", "Type Here")
+if st.button("Submit", key=2):
+    result = massage.title()
+    st.success(result)
+
+today = st.date_input("Today is ", datetime.datetime.now())
+
+the_time = st.time_input("The time is", datetime.datetime.now())
+
+st.text ("Display JSON")
+st.json({'name': 'Konstantin', 'gender': 'male'})
+
+st.text("Display Raw Code")
+st. code ("import numpy as np")
+
+with st.echo():
+    #Funny comment
+    import pandas as pd
+    df = pd.DataFrame()
 
 
-st.subheader('Prediction')
-penguins_species = np.array(['Adelie','Chinstrap','Gentoo'])
-st.write(penguins_species[prediction])
+import time
+my_bar = st.progress(0)
+for p in range(1):
+    my_bar.progress(p+1)
+    time.sleep(0)
 
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
+with st.spinner ("Waiting"):
+    time.sleep(0)
+st.success("Finished :3")
+
+#st.balloons()
+
+st.sidebar.header("About")
+st.sidebar.text("This is Streamlit tut")
+
+@st.cache
+def run_fxn():
+    return range(100)
+
+st.write(run_fxn())
